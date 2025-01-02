69c3d21d6d7df875dfd2d9874c40dc6b13430299
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-02 23:11:25 +0100

    Rectify migration error

--- database/migration/000003_create_exchange_rates_table.down.sql
+++ database/migration/000003_create_exchange_rates_table.down.sql
@@ -1,7 +1,1 @@
-CREATE TABLE exchange_rates (
-    id SERIAL PRIMARY KEY,
-    base_currency INT NOT NULL REFERENCES currencies(id),
-    target_currency INT NOT NULL REFERENCES currencies(id),
-    rate NUMERIC(12, 6) NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
+DROP TABLE IF EXISTS exchange_rates;
--- database/migration/000003_create_exchange_rates_table.up.sql
+++ database/migration/000003_create_exchange_rates_table.up.sql
@@ -1,1 +1,8 @@
-DROP TABLE IF EXISTS exchange_rates;
+CREATE TABLE exchange_rates (
+    id SERIAL PRIMARY KEY,
+    base_currency INT NOT NULL REFERENCES currencies(id),
+    target_currency INT NOT NULL REFERENCES currencies(id),
+    rate NUMERIC(12, 6) NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
