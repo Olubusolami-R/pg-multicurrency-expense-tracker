0b5cf43fb43f78bbad604cb70e01d05b631572b1
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-07 13:01:28 +0100

    Implement batch inserts

--- backend/internal/repository/currency.go
+++ backend/internal/repository/currency.go
@@ -1,12 +1,19 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strings"
 
-// Handles database operations for currency 
+	models "github.com/Olubusolami-R/multicurrency-tracker/internal/models"
+)
+
+// Handles database operations for currency
 type CurrencyRepository struct {
 	DB *sql.DB
 }
 
+// Helper function to
 func NewCurrencyRepository(db *sql.DB) CurrencyRepository{
 	return CurrencyRepository{DB:db}
 }
@@ -19,4 +26,25 @@ func (r *CurrencyRepository) InsertSingleCurrency (code string, name string) err
 	}
 	return nil
 
+}
+
+// Batch inserts
+func (r *CurrencyRepository) InsertMultipleCurrencies (currencies []models.Currency) error {
+	query := "INSERT INTO currencies (code, name) VALUES "
+	values := []interface{}{}
+	placeholders := []string{}
+
+	for i, currency := range currencies{
+		placeholders = append(placeholders, fmt.Sprintf("($%d, $%d)", i*2+1, i*2+2))
+		values = append(values, currency.Code, currency.Name)
+	}
+
+	query += strings.Join(placeholders, ",")
+
+	// Execute the query
+	_, err := r.DB.Exec(query, values...)
+	if err != nil {
+		return err
+	}
+	return nil
 }
