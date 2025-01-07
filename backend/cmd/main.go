7be8c7df2ec9e89d664721c63c3e0c35a288aa94
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-07 10:18:53 +0100

    Add godotenv

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,12 +6,18 @@ import (
 	"os"
 
 	"github.com/Olubusolami-R/multicurrency-tracker/internal/db"
+	"github.com/joho/godotenv"
 )
 
 func main(){
 
-	//Setup the database
+	// Load the .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
+	// Setup the database
 	fmt.Println(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	database, err := db.SetupDatabase(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	if err != nil {
