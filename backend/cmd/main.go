e56e521a53607ee61bc404b05b34e0bf6bfe6c6a
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-07 11:22:35 +0100

    Add insert single currency

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -18,12 +18,12 @@ func main(){
 	}
 
 	// Setup the database
-	fmt.Println(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	database, err := db.SetupDatabase(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 	defer database.Close()
+
 
 	fmt.Println("Hello there!")
 }
--- backend/internal/repository/currency.go
+++ backend/internal/repository/currency.go
@@ -1,5 +1,22 @@
 package repository
 
+import "database/sql"
+
+// Handles database operations for currency 
 type CurrencyRepository struct {
-	
+	DB *sql.DB
+}
+
+func NewCurrencyRepository(db *sql.DB) CurrencyRepository{
+	return CurrencyRepository{DB:db}
+}
+
+func (r *CurrencyRepository) InsertSingleCurrency (code string, name string) error {
+	query := "INSERT INTO currencies (code, name) VALUES ($1, $2)"
+	_,err := r.DB.Exec(query, code, name)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
