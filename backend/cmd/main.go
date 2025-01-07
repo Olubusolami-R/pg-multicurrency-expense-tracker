725855e819ae5529097cb04daf7e06b3bd28345c
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-07 10:13:45 +0100

    Restructure backend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/multicurrency-tracker/backend/internal/db"
+	"github.com/Olubusolami-R/multicurrency-tracker/internal/db"
 )
 
 func main(){
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/Olubusolami-R/multicurrency-tracker
+
+go 1.22.1
+
+require github.com/lib/pq v1.10.9
