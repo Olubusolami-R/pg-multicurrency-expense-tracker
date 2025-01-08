e423b6a6467d500441a53f9052b2c8ef7ab71625
Olubusolami Sogunle <busolasogunle@gmail.com>
2025-01-08 18:13:59 +0100

    Add currency service

---
+++ backend/internal/services/currency.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"github.com/Olubusolami-R/multicurrency-tracker/internal/models"
+	"github.com/Olubusolami-R/multicurrency-tracker/internal/repository"
+)
+
+type CurrencyService struct {
+	CurrencyRepo repository.CurrencyRepository
+}
+
+func NewCurrencyService(repo repository.CurrencyRepository) CurrencyService {
+	return CurrencyService{CurrencyRepo: repo}
+}
+
+func (s *CurrencyService) CreateSingleCurrency(currency models.Currency) error {
+	return s.CurrencyRepo.CreateSingleCurrency(currency)
+}
+
+func (s *CurrencyService) CreateMultipleCurrencies(currencies []models.Currency) error {
+	return s.CurrencyRepo.CreateMultipleCurrencies(currencies)
+}
+
+func (s *CurrencyService) GetAllCurrencies()([]models.Currency, error){
+	return s.CurrencyRepo.GetCurrencies()
+}
